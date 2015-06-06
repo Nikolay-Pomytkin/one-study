9e01e95b2fc96091e2e47fba2e8c6e6f8a0e211d
Nikolay Pomytkin <nikolayp2800@gmail.com>
2015-06-06 01:50:31 +0000

    just a crapton of shit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150605235656) do
+ActiveRecord::Schema.define(version: 20150606014931) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -60,6 +60,8 @@ ActiveRecord::Schema.define(version: 20150605235656) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "admin"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
