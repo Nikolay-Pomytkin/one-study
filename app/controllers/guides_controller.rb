d82438a18eda42bb1c1170d798312316ec418615
Nikolay Pomytkin <nikolayp2800@gmail.com>
2015-06-12 01:25:14 +0000

    new guides support

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -8,7 +8,8 @@ class GuidesController < ApplicationController
 
   # GET /guides/new
   def new
-    @guide = Guide.new
+    @course = Course.find_by_id(params[:course_id])
+    @guide = Guide.new(teacher_id: params[:teacher_id], user_id: params[:user_id])
   end
 
   # GET /guides/1/edit
