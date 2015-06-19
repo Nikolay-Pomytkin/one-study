c2e66a10a8af49a28878bc1863e722b728335e49
Nikolay Pomytkin <nikolayp2800@gmail.com>
2015-06-19 19:20:07 -0400

    generated avatar for user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150618155213) do
+ActiveRecord::Schema.define(version: 20150619231938) do
 
   create_table "accesses", force: :cascade do |t|
     t.integer  "guide_id"
@@ -71,6 +71,10 @@ ActiveRecord::Schema.define(version: 20150618155213) do
     t.boolean  "admin"
     t.string   "name"
     t.text     "description"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
