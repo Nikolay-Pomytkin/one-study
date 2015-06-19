12cba75f7d670e8812d6c773e795d65c55b416fe
Nikolay Pomytkin <nikolayp2800@gmail.com>
2015-06-19 18:07:37 -0400

    integration of users#show into app

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
 
+  #Authentication and profile page
+  devise_for :users
+  get 'users/:id' => 'users#show'
+
+  #Home page
   root "pages#welcome"
 
   resources :guides
-
   resources :courses
-
   resources :teachers
-
   resources :schools
-
   resources :pages
 
+  #STATIC PAGES
   match '/about' => 'pages#about', via: :all
-
   match '/welcome' => 'pages#welcome', via: :all
-
   match '/contact_us' => 'pages#contact_us', via: :all
 
 end
