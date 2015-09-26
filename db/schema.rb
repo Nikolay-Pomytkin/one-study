e30829a50c1ef3b92e4c716ae30031ac71852ef4
Nikolay Pomytkin <nikolayp2800@gmail.com>
2015-09-25 21:01:16 -0700

    rake db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150925214904) do
+ActiveRecord::Schema.define(version: 20150926033636) do
 
   create_table "schools", force: true do |t|
     t.string   "name"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20150925214904) do
     t.string   "city"
     t.string   "state"
     t.integer  "zipcode"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "teachers", force: true do |t|
+    t.integer  "school_id"
+    t.string   "name"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
