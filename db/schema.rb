f9da7e08670df3b3b77a03a258f7a3b7d184cc7b
Nikolay Pomytkin <nikolayp2800@gmail.com>
2015-09-30 18:31:42 -0700

    generated courses controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151001012557) do
+ActiveRecord::Schema.define(version: 20151001013102) do
+
+  create_table "courses", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "teacher_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "guides", force: true do |t|
     t.string   "title"
