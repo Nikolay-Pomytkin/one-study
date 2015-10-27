23a6d3a6ef3c1454dac26425860768af84ea3ce0
Nikolay Pomytkin <nikolayp2800@gmail.com>
2015-10-27 18:01:49 -0400

    generated course scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027220024) do
+ActiveRecord::Schema.define(version: 20151027220121) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "teacher_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "schools", force: :cascade do |t|
     t.string   "name"
