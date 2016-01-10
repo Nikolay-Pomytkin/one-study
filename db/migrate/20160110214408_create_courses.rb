302058f4ad76d8b98cb108aae7bfd3c3df98236e
Nikolay Pomytkin <nikolayp2800@gmail.com>
2016-01-10 21:44:40 +0000

    generate course scaffold

---
+++ db/migrate/20160110214408_create_courses.rb
@@ -0,0 +1,11 @@
+class CreateCourses < ActiveRecord::Migration
+  def change
+    create_table :courses do |t|
+      t.string :name
+      t.string :description
+      t.integer :teacher_id
+
+      t.timestamps null: false
+    end
+  end
+end
