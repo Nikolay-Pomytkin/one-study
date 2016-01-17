ac8f7d2d30bc2ef87f1441b666bfcdff39dc502e
Nikolay Pomytkin <nikolayp2800@gmail.com>
2016-01-17 03:34:25 +0000

    for moss

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,4 @@ class User < ActiveRecord::Base
   
   has_many :guides
   
-  # Parameter sanitation
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-      user_params.permit(:username, :email)
-    end
-    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit(:username, :email, :name)
-    end
-  end
 end
