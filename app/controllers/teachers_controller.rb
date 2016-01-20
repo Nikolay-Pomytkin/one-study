a42a39fb3ac6c042b1856a8376204f81b5ec6a8a
Nikolay Pomytkin <nikolayp2800@gmail.com>
2016-01-20 20:18:46 +0000

    work on teachers styling

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -10,6 +10,7 @@ class TeachersController < ApplicationController
   # GET /teachers/1
   # GET /teachers/1.json
   def show
+    @courses = Teacher.courses.all.first(5)
   end
 
   # GET /teachers/new
