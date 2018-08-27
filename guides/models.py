c5d3c28b6c58c42aa3de2870d226a467de057890
Nikolay Pomytkin <nikolayp2800@gmail.com>
2018-08-27 17:22:57 -0400

    fixed all bugs regarding models including rearranging (scripting languages lul)

--- guides/models.py
+++ guides/models.py
@@ -1,6 +1,47 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+#superuser username nikolayp email nik@pomytkin.com password blungoblast
+
+class School(models.Model):
+    name = models.CharField(max_length=150)
+    location = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="schools_created")
+    class Meta:
+        ordering = ('-created',)
+    def __str__():
+        return self.title
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    school = models.ForeignKey(School, on_delete=models.CASCADE, related_name="teachers")
+    # also add fields for popularity (clicks, upvotes, etc)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="teachers_created")
+    class Meta:
+        ordering = ('-created',)
+    def __str__():
+        return self.title
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name="courses")
+    # will probably add fields regarding popularity of the course (to filter out stupidly created courses)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="courses_created")
+    class Meta:
+        ordering = ('-created',)
+    def __str__():
+        return self.title
+
 
 class Guide(models.Model):
     STATUS_CHOICES = (
@@ -15,43 +56,6 @@ class Guide(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='public')
     class Meta:
-        ordering = '-publish'
-    def __str__():
-        return self.title
-
-class Course(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name="courses")
-    # will probably add fields regarding popularity of the course (to filter out stupidly created courses)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey(User, on_delete=SET_NULL, related_name="courses_created")
-    class Meta:
-        ordering = '-publish'
-    def __str__():
-        return self.title
-
-class Teacher(models.Model):
-    name = models.CharField(max_length=150)
-    description = models.TextField()
-    school = models.ForeignKey(School, on_delete=models.CASCADE, related_name="teachers")
-    # also add fields for popularity (clicks, upvotes, etc)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey(User, on_delete=SET_NULL, related_name="courses_created")
-    class Meta:
-        ordering = '-publish'
-    def __str__():
-        return self.title
-
-class School(models.Model):
-    name = models.CharField(max_length=150)
-    location = models.CharField(max_length=250)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey(User, on_delete=SET_NULL, related_name="courses_created")
-    class Meta:
-        ordering = '-publish'
+        ordering = ('-created',)
     def __str__():
         return self.title
